d3008eee7f75927d4529b4bcc6f94c3c89637906
Łukasz Ziobroń <lukasz@coders.school>
2019-05-02 13:09:36 +0200

    ActiveObject added

---
+++ 10_concurrent_design_patterns/exercises/active_object/ActiveObject.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "Object.hpp"
+#include <memory>
+#include <future>
+#include <mutex>
+#include <iostream>
+
+class ActiveObject
+{
+    std::unique_ptr<Object> object_;
+
+public:
+    ActiveObject(std::unique_ptr<Object> obj)
+        : object_(std::move(obj))
+    {}
+
+    std::future<void> push(const int value)
+    {
+        // TODO: Run push() on object_ asynchronously, print this_thread::get_id(), return a future
+        return {};
+    }
+
+    std::future<int> pop()
+    {
+        // TODO: Run pop() on object_ asynchronously, print this_thread::get_id(), return a future
+        return {};
+    }
+};
