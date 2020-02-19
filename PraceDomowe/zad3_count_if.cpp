21a9022e4885c9f2415e0b891a31db9d4437cba6
Krzysztof Doroszko <doroszko.k@gmail.com>
2020-02-19 22:09:04 +0100

    Add code for empty container case, add .gitignore, delete type T from template

--- PraceDomowe/zad3_count_if.cpp
+++ PraceDomowe/zad3_count_if.cpp
@@ -1,14 +1,17 @@
-#include <iostream>
-#include <thread>
-#include <numeric>
 #include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <thread>
 #include <vector>
 
 
 constexpr size_t minSize = 10000;
 
-template <typename It, typename Predicate, typename T>
-T p_count_if(It first, It last, Predicate pred){
+template <typename It, typename Predicate>
+typename std::iterator_traits<It>::difference_type p_count_if(It first, It last, Predicate pred){
+
+    if(std::distance(first, last) == 0)
+        return 0;
 
     const size_t dataSize = std::distance(first, last);
 	if (dataSize < minSize){
@@ -19,7 +22,7 @@ T p_count_if(It first, It last, Predicate pred){
 	const size_t neededThreads = std::min(dataSize / minSize, hardwareThread);
 	const size_t chunkSize = dataSize / neededThreads;
 	std::vector<std::thread> threads(neededThreads - 1);
-	std::vector<T> countedResults(neededThreads);
+    std::vector<int> countedResults(neededThreads);
 
 
 }
@@ -47,6 +50,10 @@ int main(){
  
     int counter = std::count_if(v.begin(), v.end(), [](int i){return i % 3 == 0;});
     std::cout << "Amout of counted numbers divided by 3: " << counter << '\n';
+
+    std::vector<int> emptyVector;
+    auto emptyResult = p_count_if(emptyVector.begin(), emptyVector.end(), [](int i){return i % 3 == 0;});
+    std::cout << "Result of p_count_if for empty vector: " << emptyResult << '\n';
 
     return 0;
 }
