78f719d521f647800981f1c559cb998041e6df2d
MonikaZ <monika.zelechowska@o2.pl>
2020-02-22 17:55:22 +0100

    Change of code formatting

--- PraceDomowe/zad3_count_if.cpp
+++ PraceDomowe/zad3_count_if.cpp
@@ -14,13 +14,13 @@ constexpr size_t minSize = 10000;
 
 template <typename It, typename Predicate>
 typename std::iterator_traits<It>::difference_type p_count_if(It first, It last, Predicate pred){
-
     if(std::distance(first, last) == 0)
         return 0;
+
     const size_t dataSize = std::distance(first, last);
-	if (dataSize < minSize){
+
+	if (dataSize < minSize)
 		return std::count_if(first, last, pred);
-    }
 
 	const size_t hardwareThread = std::thread::hardware_concurrency();
 	const size_t neededThreads = std::min(dataSize / minSize, hardwareThread);
@@ -29,15 +29,14 @@ typename std::iterator_traits<It>::difference_type p_count_if(It first, It last,
     std::vector<int> results(neededThreads);
 
     auto threadBegin = first;
-   
     for (size_t i = 0; i < neededThreads - 1; ++i) {
-            auto threadEnd = std::next(threadBegin, chunkSize);
-            threads[i] = std::thread([](It firstIterator, It lastIterator, Predicate threadPred, int& counter)
-                {
-                    counter = std::count_if(firstIterator, lastIterator, threadPred);
-                }, threadBegin, threadEnd, pred, std::ref(results[i]));
-            threadBegin = threadEnd;
-        }
+        auto threadEnd = std::next(threadBegin, chunkSize);
+        threads[i] = std::thread([](It firstIterator, It lastIterator, Predicate threadPred, int& counter)
+            {
+                counter = std::count_if(firstIterator, lastIterator, threadPred);
+            }, threadBegin, threadEnd, pred, std::ref(results[i]));
+        threadBegin = threadEnd;
+    }
 
     results[neededThreads - 1] = std::count_if(threadBegin, last, pred);
     std::for_each(std::begin(threads), std::end(threads), std::mem_fn(&std::thread::join));
@@ -49,25 +48,8 @@ typename std::iterator_traits<It>::difference_type p_count_if(It first, It last,
     return counter;
 }
 
-/* cppreference implementation
-
-template<class InputIt, class UnaryPredicate>
-typename iterator_traits<InputIt>::difference_type
-    count_if(InputIt first, InputIt last, UnaryPredicate p)
-{
-    typename iterator_traits<InputIt>::difference_type ret = 0;
-    for (; first != last; ++first) {
-        if (p(*first)) {
-            ret++;
-        }
-    }
-    return ret;
-}
-*/
-
 template <typename It, typename Predicate>
-void testCompareResultsOfp_count_ifWithSTLcount_if(It first, It last, Predicate pred)
-{
+void testCompareResultsOfp_count_ifWithSTLcount_if(It first, It last, Predicate pred){
     bool sameResult = p_count_if(first, last, pred) == std::count_if(first, last, pred);
 
     if(sameResult)
@@ -76,10 +58,7 @@ void testCompareResultsOfp_count_ifWithSTLcount_if(It first, It last, Predicate 
 }
 
 int main(){
-
-
     std::vector<int> v{ 1, 9, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10 };
- 
     int counter = std::count_if(v.begin(), v.end(), [](int i){return i % 3 == 0;});
     std::cout << "Amout of counted numbers divided by 3: " << counter << '\n';
 
@@ -98,11 +77,12 @@ int main(){
     std::count_if(testVector.begin(), testVector.end(), [](int i){return i % 3 == 0;});
     auto stopSTL_count_if = std::chrono::steady_clock::now();
 
-    std::cout << "Pararel algorithm lasted: "
+    std::cout << "\n--- Algorithm running time calculation ---\n";
+    std::cout << "Parallel algorithm for 1'000'000 element vector lasted: "
             << std::chrono::duration_cast<std::chrono::microseconds>(stopp_count_if - startp_count_if).count()
             << "us\n";
 
-    std::cout << "STL algorithm lasted: "
+    std::cout << "STL algorithm for 1'000'000 element vector lasted: "
             << std::chrono::duration_cast<std::chrono::microseconds>(stopSTL_count_if - startSTL_count_if).count()
             << "us\n";
 
@@ -111,11 +91,15 @@ int main(){
     std::set<std::string> testSet {"abc", "edf", "abc", "xyz"};
     std::list<int> testList;
 
+    std::cout << "\n--- Tests ---\n";
+    std::cout << "Vector case - ";
     testCompareResultsOfp_count_ifWithSTLcount_if(testVector.begin(), testVector.end(), [](int i){return i % 3 == 0;});
+    std::cout << "Array case - ";
     testCompareResultsOfp_count_ifWithSTLcount_if(testArray.begin(), testArray.end(), [](int i){return i % 11 == 0;});
+    std::cout << "Set case - ";
     testCompareResultsOfp_count_ifWithSTLcount_if(testSet.begin(), testSet.end(), [](std::string s){return s == "abc";});
+    std::cout << "List case - ";
     testCompareResultsOfp_count_ifWithSTLcount_if(testList.begin(), testList.end(), [](int i){return i % 3 == 0;});
-
-
+    
     return 0;
 }
