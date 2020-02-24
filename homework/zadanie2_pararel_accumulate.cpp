06a0ae73a2be02d73df2d4a7d479d9ac32e8527c
piotrkowandy <piotr.kowandy@o2.pl>
2020-02-24 21:38:01 +0100

    zadanie2 poprawione

--- homework/zadanie2_pararel_accumulate.cpp
+++ homework/zadanie2_pararel_accumulate.cpp
@@ -5,36 +5,38 @@
 #include <vector>
 #include <chrono>
 #include <functional>
+#include <cassert>
 
 
 template <typename IT, typename T>
 T pararelAccumulate(IT first, IT last, T init) {
-    if (!init) {
         const size_t size = std::distance(first, last);
-        const size_t hardware_threads = std::thread::hardware_concurrency() > 0 ? std::thread::hardware_concurrency() : 2;
-        const size_t chunkSize = size / hardware_threads;
-        std::vector<std::thread> threads(hardware_threads - 1);
-        std::vector<T> results(hardware_threads);
 
-        auto begin = first;
-        for (size_t i = 0; i < hardware_threads - 1; ++i) {
-            auto end = std::next(begin, chunkSize);
-            threads[i] = std::thread([](IT first, IT last, T& result)
-                {
-                    result = std::accumulate(first, last, T{});
-                }, begin, end, std::ref(results[i]));
-            begin = end;
-        }
+    if (!size)
+        return init;
 
-        results[hardware_threads - 1] = std::accumulate(begin, last, T{});
+    const size_t hardware_threads = std::thread::hardware_concurrency() > 0 ? std::thread::hardware_concurrency() : 2;
+    const size_t chunkSize = size / hardware_threads;
+    std::vector<std::thread> threads(hardware_threads - 1);
+    std::vector<T> results(hardware_threads);
 
-        for (auto && t : threads) {
-            t.join();
-        }
-
-        return std::accumulate(std::begin(results), std::end(results), init);
+    auto begin = first;
+    for (size_t i = 0; i < hardware_threads - 1; ++i) {
+        auto end = std::next(begin, chunkSize);
+        threads[i] = std::thread([](IT first, IT last, T& result)
+            {
+                result = std::accumulate(first, last, T{});
+            }, begin, end, std::ref(results[i]));
+        begin = end;
     }
-    return init;
+
+    results[hardware_threads - 1] = std::accumulate(begin, last, T{});
+
+    for (auto && t : threads) {
+        t.join();
+    }
+
+    return std::accumulate(std::begin(results), std::end(results), init);
 }
 
 
@@ -44,21 +46,22 @@ int main() {
     std::generate(begin(vec), end(vec), [x{ 0 }]()mutable{ return ++x; });
 
     std::cout<< "\nStart of pararel accumulate. \n";
-    auto start = std::chrono::steady_clock::now();
-    pararelAccumulate(std::begin(vec), std::end(vec), 0);
-    auto stop = std::chrono::steady_clock::now();
+    auto start = std::chrono::high_resolution_clock::now();
+    auto sumOfvectorParalel = pararelAccumulate(std::begin(vec), std::end(vec), 0);
+    auto stop = std::chrono::high_resolution_clock::now();
     std::cout<< "End of paralel accumulate. \n";
     std::cout<< "Time of pararel accumulate function: " 
              << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << "\n\n"; 
 
     std::cout<< "Start of std::accumulate. \n";
-    start = std::chrono::steady_clock::now();
-    std::accumulate(std::begin(vec), std::end(vec), 0);
-    stop = std::chrono::steady_clock::now();
+    start = std::chrono::high_resolution_clock::now();
+    auto sumOfvec = std::accumulate(std::begin(vec), std::end(vec), 0);
+    stop = std::chrono::high_resolution_clock::now();
     std::cout<< "End of std::accumulate. \n";
     std::cout<< "Time of std::accumulate function: "
              << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << "\n\n";
-    
+
+    assert(sumOfvectorParalel == sumOfvec);
 
     return 0;
 }
