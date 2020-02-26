5291de656ea60730c2b9207d9de0dc3b2995d0c7
piotrkowandy <piotr.kowandy@o2.pl>
2020-02-26 20:39:44 +0100

    zadanie 3 z std::execution::par + czasy

--- homework/zadanie3_parallel_count_if.cpp
+++ homework/zadanie3_parallel_count_if.cpp
@@ -6,7 +6,7 @@
 #include <chrono>
 #include <functional>
 #include <cassert>
-
+#include <execution>
 
 template <typename IT, typename Pre>
 int parallelCountIf(IT first, IT last, Pre predicat) {
@@ -45,7 +45,8 @@ int main() {
 
     std::vector<int> vec{3,4,5,2,10,44,65,34,1,0,15,20,40,60,33,3,8,9,0,1,111,10};
     std::vector<int> vec1m(1'000'000);
-    std::generate(vec1m.begin(), vec1m.end(), [n=0](){return n+5;});
+    size_t n =0;
+    std::generate(vec1m.begin(), vec1m.end(), [&n](){return n+=5;});
     
     auto predicat = [](int& x){return x%5 == 0 and x != 0; };
 
@@ -69,7 +70,17 @@ int main() {
              << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << "\n\n";
     std::cout<< "Results of non parallel: " << dividedBy5 << "\n\n";
 
+    std::cout<< "Start of std::count_if with std::execution::par. \n";
+    start = std::chrono::high_resolution_clock::now();
+    auto dividedBy5_exec = std::count_if(std::execution::par, std::begin(vec), std::end(vec), predicat);
+    stop = std::chrono::high_resolution_clock::now();
+    std::cout<< "End of std::count_if with std::execution::par. \n";
+    std::cout<< "Time of std::count_if function with std::execution::par: "
+             << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << "\n\n";
+    std::cout<< "Results of non parallel with std::execution: " << dividedBy5_exec << "\n\n";
+
     assert(dividedBy5Parallel == dividedBy5);
+    assert(dividedBy5Parallel == dividedBy5_exec);
 
     std::cout<< "Algorithms are working on big amount of elements in vector. \n\n";
 
@@ -82,6 +93,15 @@ int main() {
              << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << "\n\n";
     std::cout<< "Results of parallel: " << dividedBy5Parallel << "\n\n";
 
+    std::cout<< "Start of std::count_if with std::execution::par. \n";
+    start = std::chrono::high_resolution_clock::now();
+    dividedBy5_exec = std::count_if(std::execution::par, std::begin(vec1m), std::end(vec1m), predicat);
+    stop = std::chrono::high_resolution_clock::now();
+    std::cout<< "End of std::count_if with std::execution::par. \n";
+    std::cout<< "Time of std::count_if function with std::execution: "
+             << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << "\n\n";
+    std::cout<< "Results of non parallel: " << dividedBy5_exec << "\n\n";
+
     std::cout<< "Start of std::count_if. \n";
     start = std::chrono::high_resolution_clock::now();
     dividedBy5 = std::count_if(std::begin(vec1m), std::end(vec1m), predicat);
@@ -91,9 +111,54 @@ int main() {
              << std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count() << "\n\n";
     std::cout<< "Results of non parallel: " << dividedBy5 << "\n\n";
 
+
     assert(dividedBy5Parallel == dividedBy5);
+    assert(dividedBy5Parallel == dividedBy5_exec);
 
     return 0;
 }
 
+/*
+Algorithms are working on small amount of elements in vector. 
 
+
+Start of pararel count_if. 
+End of parallel count_if. 
+Time of parallel count_if function: 516
+
+Results of parallel: 8
+
+Start of std::count_if. 
+End of std::count_if. 
+Time of std::count_if function: 1
+
+Results of non parallel: 8
+
+Start of std::count_if with std::execution::par. 
+End of std::count_if with std::execution::par. 
+Time of std::count_if function with std::execution::par: 840
+
+Results of non parallel with std::execution: 8
+
+Algorithms are working on big amount of elements in vector. 
+
+
+Start of pararel count_if. 
+End of parallel count_if. 
+Time of parallel count_if function: 11984
+
+Results of parallel: 1000000
+
+Start of std::count_if with std::execution::par. 
+End of std::count_if with std::execution::par. 
+Time of std::count_if function with std::execution: 9668
+
+Results of non parallel: 1000000
+
+Start of std::count_if. 
+End of std::count_if. 
+Time of std::count_if function: 16252
+
+Results of non parallel: 1000000
+
+*/
