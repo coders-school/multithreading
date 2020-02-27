7409fffac1daf4e72916173c115866d7251e175e
Zych Jakub <jakub.zych@tieto.com>
2020-02-27 15:18:25 +0100

    [WIP] definitions of Table, Fork and Philosopher classes

--- 01_homework/inc/Philosopher.hpp
+++ 01_homework/inc/Philosopher.hpp
@@ -1,3 +1,15 @@
 #include "Table.hpp"
+#include <thread>
 
-class Philosopher;
+class Philosopher {
+    Table const &     table_;
+    Fork&             fork_left_;
+    Fork&             fork_right_;
+    std::thread       thread_;
+
+public:
+    Philosopher(Table const &table, Fork& f_left, Fork& f_right, std::string name) noexcept;
+    ~Philosopher();
+
+    void dine();
+};
