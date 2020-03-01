6a3ab39a3bc91f2cd11fb974191ac1a15fde1449
Martyna <martynalecka.ml@gmail.com>
2020-03-01 17:18:05 +0100

    Added flags in CMake; fix bug in parallel_accumulate: added gnu parallel

--- homework/zad2/parallel_accumulate.cpp
+++ homework/zad2/parallel_accumulate.cpp
@@ -6,12 +6,13 @@
 #include <numeric>
 #include <functional>
 #include <execution>
+#include <parallel/numeric>
 
 
 template <typename Iterator, typename T>
 T p_accumulate(Iterator first, Iterator last, T init){
     const size_t size = std::distance(first, last);
-    if(size == 0)
+    if(size == 0) // Co jeżeli: The value may be negative if random-access iterators are used and first is reachable from last (since C++11)? dodać warunek na łapanie < od 0
         return init;
     const size_t hardware_threads = std::thread::hardware_concurrency() != 0 ? std::thread::hardware_concurrency() : 2;
     const size_t block_size_for_thread = size / hardware_threads;
@@ -23,7 +24,7 @@ T p_accumulate(Iterator first, Iterator last, T init){
     Iterator begin_it = first;
     for(size_t i = 0; i < used_threads - 1; ++i)
     {
-        Iterator end = std::next(begin_it, data_chunk);
+        Iterator end = std::next(begin_it, data_chunk); //Można jawnie skonwertować data_chunk na iterator z template - Iterator? Przy static_cast<Iterator>(data_chunk) rzuca że nie może zmaczować, a tak to warningi rzuca o niejawnej konwersji :/
         threads[i] = std::thread(
                 [](Iterator begin_it, Iterator end_it, T& result){
                     result = std::accumulate(begin_it, end_it, T{});
@@ -46,27 +47,27 @@ int main() {
 
     auto result = p_accumulate(vec.begin(), vec.end(), 0);
     std::cout << "Compare results from created function: " << result << std::endl;
-    std::cout << "with STL" << std::accumulate(begin(vec), end(vec), 0) <<std::endl; //zagnieżdzanie namespaceow dlatego std:: nie trzeba przy begin i end
+    std::cout << " with STL " << std::accumulate(begin(vec), end(vec), 0) <<std::endl; //zagnieżdzanie namespaceow dlatego std:: nie trzeba przy begin i end
 
     auto start = std::chrono::high_resolution_clock::now();
     result = p_accumulate(vec.begin(), vec.end(), 0);
     auto stop = std::chrono::high_resolution_clock::now();
 
     auto start1 = std::chrono::high_resolution_clock::now();
-    auto result1 = std::accumulate(begin(vec), end(vec), 0) <<std::endl;
+    auto result1 = std::accumulate(begin(vec), end(vec), 0);
     auto stop1 = std::chrono::high_resolution_clock::now();
 
     auto start2 = std::chrono::high_resolution_clock::now();
-    auto result2 = std::accumulate(std::execution::par, begin(vec), end(vec), 0) <<std::endl;
+    auto result2 = __gnu_parallel::accumulate(begin(vec), end(vec), 0);
     auto stop2 = std::chrono::high_resolution_clock::now();
 
 
     std::chrono::duration<float> duration = stop - start;
-    std::cout << "The duration of the p_accumulate function created: " << duration.count() << "\n Result: " << result << '\n';
+    std::cout << "The duration of the p_accumulate function created: \t" << duration.count() << "\n Result: " << result << '\n';
 
     std::chrono::duration<float> duration1 = stop1 - start1;
-    std::cout << "The duration of the STL accumulate function: " << duration1.count() << "\n Result: " << result1 << '\n';
+    std::cout << "The duration of the STL accumulate function: \t" << duration1.count() << "\n Result: " << result1 << '\n';
 
     std::chrono::duration<float> duration2 = stop2 - start2;
-    std::cout << "The duration of the STL accumulate with execution::par function: " << duration2.count() << "\n Result: " << result2 << '\n';
+    std::cout << "The duration of the STL accumulate with execution::par function: \t" << duration2.count() << "\n Result: " << result2 << '\n';
 }
