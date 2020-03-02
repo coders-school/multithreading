77a4bcf6db7d3e8d3ee0b0fa7b8740ebefe617a2
mkijew <mkijew@poczta.fm>
2020-03-02 23:38:17 +0100

    zad7: version using threads

--- 07_future_promise/homework/notification_sender.cxx
+++ 07_future_promise/homework/notification_sender.cxx
@@ -1,8 +1,16 @@
+#include <exception>
+#include <functional>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <sstream>
 #include <vector>
 
 #include <libmail/include/send_mail.h>
 #include <libmail/include/randomize_results.h>
 
+decltype(std::chrono::steady_clock::now()) start;
+decltype(start) stop;
 
 int main()
 {
@@ -17,4 +25,41 @@ int main()
 	std::string message = "build failed";
 
 	// ...
+
+	std::vector<std::thread> threads;
+
+	start = std::chrono::steady_clock::now();
+
+	// start threads
+	for ( std::string str : responsible_devs )
+	{
+		threads.push_back( std::thread([]( std::string strDev )
+			{
+				try
+				{
+					bool bResult = libmail::send_mail(strDev, "message");
+					std::stringstream ss;
+					ss << "> Sending mail to: " << strDev << " - ";
+					ss << ((bResult) ? "OK" : "FAIL");
+					ss << std::endl;
+					std::cout << ss.str();
+				}
+				catch( const std::exception& e )
+				{
+					std::stringstream ss;
+					ss << "> Sending mail to: <exception> " << e.what();
+					ss << std::endl;
+					std::cout << ss.str();
+				}	 				
+			}, str));
+	}
+
+	// join thread
+	std::for_each( std::begin( threads ), std::end( threads ), std::mem_fn( &std::thread::join ) );
+
+	stop = std::chrono::steady_clock::now();
+
+	std::cout << "\nSending mails time: " 
+		<< std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() 
+		<< "ms" << std::endl;
 }
