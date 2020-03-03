34d7b6421461b70f20b8fc04dd58781b8500f2ac
mkijew <mkijew@poczta.fm>
2020-03-03 21:29:07 +0100

    zad7: version using future and promise

--- 07_future_promise/homework/notification_sender.cxx
+++ 07_future_promise/homework/notification_sender.cxx
@@ -1,13 +1,19 @@
 #include <exception>
 #include <functional>
+#include <future>
 #include <iostream>
 #include <chrono>
 #include <thread>
 #include <sstream>
 #include <vector>
 
-#include <libmail/include/send_mail.h>
-#include <libmail/include/randomize_results.h>
+// version 1
+// #include <libmail/include/send_mail.h>
+// #include <libmail/include/randomize_results.h>
+
+// version2
+#include <libmail2.0/include/send_mail.h>
+#include <libmail2.0/include/randomize_results.h>
 
 decltype(std::chrono::steady_clock::now()) start;
 decltype(start) stop;
@@ -27,35 +33,63 @@ int main()
 	// ...
 
 	std::vector<std::thread> threads;
+	std::vector<std::future<bool>> futures;
 
 	start = std::chrono::steady_clock::now();
 
 	// start threads
-	for ( std::string str : responsible_devs )
+	for ( std::string strDev : responsible_devs )
 	{
-		threads.push_back( std::thread([]( std::string strDev )
+		auto threadSendMail = []( std::string dev, std::string msg, std::promise<bool> promise )
+		{
+			try
 			{
-				try
-				{
-					bool bResult = libmail::send_mail(strDev, "message");
-					std::stringstream ss;
-					ss << "> Sending mail to: " << strDev << " - ";
-					ss << ((bResult) ? "OK" : "FAIL");
-					ss << std::endl;
-					std::cout << ss.str();
-				}
-				catch( const std::exception& e )
-				{
-					std::stringstream ss;
-					ss << "> Sending mail to: <exception> " << e.what();
-					ss << std::endl;
-					std::cout << ss.str();
-				}	 				
-			}, str));
+				// version 1.0
+				// bool bResult = libmail::send_mail(dev, msg);
+				// promise.set_value( bResult );
+
+				// version 2.0
+				libmail::send_mail(dev, msg);
+				promise.set_value( true );
+			}
+			catch( ... )
+			{
+				promise.set_exception( std::current_exception() );
+			}
+		};
+
+		std::promise<bool> p;
+		std::future<bool> f = p.get_future();
+
+		futures.push_back( std::move(f) );
+		threads.push_back( std::thread( threadSendMail, strDev, "message", std::move(p) ) );
 	}
 
 	// join thread
 	std::for_each( std::begin( threads ), std::end( threads ), std::mem_fn( &std::thread::join ) );
+
+	// print results
+	for ( unsigned int i = 0; i < responsible_devs.size(); ++i )
+	{
+		try
+		{
+			// version 1.0
+			// bool bResult = futures[i].get();
+
+			// version 2.0
+			bool bResult = true;
+			futures[i].get();
+
+			std::cout << "> Sending mail to: " << responsible_devs[i] << " - ";
+			std::cout << ((bResult) ? "OK" : "FAIL");
+			std::cout << std::endl;
+		}
+		catch ( const std::exception& e )
+		{
+			std::cout << "> Sending mail to: " << responsible_devs[i] << " <exception> " << e.what();
+			std::cout << std::endl;
+		}
+	}
 
 	stop = std::chrono::steady_clock::now();
 
