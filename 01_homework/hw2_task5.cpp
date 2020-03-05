e84c409bfde1f6581cf8716cfaa2faf82342ddce
Jakub Zych <jzych89@gmail.com>
2020-03-05 14:08:35 +0100

    Time measure fix and alignas added

--- 01_homework/hw2_task5.cpp
+++ 01_homework/hw2_task5.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <chrono>
 #include <array>
+#include <vector>
 #include <thread>
 #include <numeric>
 
-constexpr int reps = 1000000;
+constexpr int reps = 2000000;
 
 void stress_function(int a, int &b) {
     for(int i = 0; i < reps; i++) {
@@ -12,37 +13,50 @@ void stress_function(int a, int &b) {
     }
 }
 
+struct AlignedInt {
+    alignas(64) int value[6];
+};
+
 int main() {
     std::array<int, 66> data;
     std::iota(data.begin(), data.end(), 1);
 
-    std::cout << "========== False sharing ===========\n";
+    AlignedInt a;
+
+    std::cout << "=========== False sharing ===========\n";
     auto start = std::chrono::high_resolution_clock::now();
-    std::thread t1(stress_function, data[1], std::ref(data[2]));
-    std::thread t2(stress_function, data[5], std::ref(data[6]));
+    std::thread t1(stress_function, data[0], std::ref(data[1]));
+    std::thread t2(stress_function, data[2], std::ref(data[3]));
     t1.join();
     t2.join();
     auto stop = std::chrono::high_resolution_clock::now();
-
     std::chrono::duration<float> duration = stop - start;
     std::cout << "Duration: " << duration.count() << '\n';
+
+    std::cout << "====== Fixed sharing - spacing ======\n";
+    start = std::chrono::high_resolution_clock::now();
     std::thread t3(stress_function, data[1], std::ref(data[2]));
     std::thread t4(stress_function, data[65], std::ref(data[66]));
     t3.join();
     t4.join();
-    std::cout << "====== Fixed sharing ======\n";
-    start = std::chrono::high_resolution_clock::now();
-
-
     stop = std::chrono::high_resolution_clock::now();
-
     duration = stop - start;
     std::cout << "Duration: " << duration.count() << '\n';
 
-    std::cout << "======== Sequentional ========\n";
+    std::cout << "====== Fixed sharing - alignas ======\n";
+    start = std::chrono::high_resolution_clock::now();
+    std::thread t5(stress_function, a.value[0], std::ref(a.value[1]));
+    std::thread t6(stress_function, a.value[4], std::ref(a.value[5]));
+    t5.join();
+    t6.join();
+    stop = std::chrono::high_resolution_clock::now();
+    duration = stop - start;
+    std::cout << "Duration: " << duration.count() << '\n';
+
+    std::cout << "=========== Sequentional ============\n";
     start = std::chrono::high_resolution_clock::now();
     stress_function(data[1], std::ref(data[2]));
-    stress_function(data[5], std::ref(data[6]));
+    stress_function(data[3], std::ref(data[4]));
     stop = std::chrono::high_resolution_clock::now();
     duration = stop - start;
     std::cout << "Duration: " << duration.count() << '\n';
@@ -50,11 +64,11 @@ int main() {
 /*****************************************************************************
  *
  * Zjawisko false sharingu występuje poniewaz watki probuja dostac sie do tej
- * samej komorki pamieci mimo zeznajduja sie tam dwie niezalezne zmienne. Zeby
+ * samej linii cache'u mimo zeznajduja sie tam dwie niezalezne zmienne. Zeby
  * uniknac tego zjawiska trzeba sie upewnic ze dane na ktorych pracuja osobne
  * watki znajduja sie w osobnych komorkach pamieci. Ich rozsuwanie (padding
  * można zrealizować poprzez wstawianie dodatkowcyh sztucznych zmiennych albo
- * za pomoca odpowiednich komend kompilatora (__declspec (align(64)))
+ * za pomoca odpowiednich komend kompilatora (__declspec (align(64)) lub funkcji alignas(64))
  *
  *****************************************************************************/
 
