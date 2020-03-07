4da7fcef7c572659a50f82dcd14ffff825b8a1e9
kawapa <kawapa@outlook.com>
2020-03-07 17:45:56 +0100

    Another version

--- 00_homework_kawapa/Z4.cpp
+++ 00_homework_kawapa/Z4.cpp
@@ -5,61 +5,78 @@
 #include <thread>
 #include <vector>
 
-using namespace std::chrono_literals;
+constexpr size_t n = 15;
 
-std::mutex forks[5];
+std::mutex forks[n];
 
 struct Philosopher
 {
-    std::string name_;
+    int id_;
     int forkIndexLeft_;
     int forkIndexRight_;
     bool hungry_ = true;
 
     void dine()
     {
-        contemplate();
-        eat();
+        wait();
+
+        while (hungry_)
+        {
+            contemplate();
+            eat();
+        }
     }
 
     void eat()
     {
-        std::stringstream ss;
-        std::unique_lock<std::mutex> l1(forks[forkIndexLeft_], std::defer_lock);
-        std::
-        std::unique_lock<std::mutex> l2(forks[forkIndexRight_], std::defer_lock);
-        std::lock(l1, l2);
-        
-        ss << name_ << "is eating\n";
-        std::cout << ss.str();
-    }
+        wait();
+        std::lock(forks[forkIndexLeft_], forks[forkIndexRight_]);
+ 
+        std::lock_guard<std::mutex> mutex_l(forks[forkIndexLeft_], std::adopt_lock);
+        print("has just picked up the left fork");
+
+        wait();
+        std::lock_guard<std::mutex> mutex_r(forks[forkIndexRight_], std::adopt_lock);
+        print("has just picked up the right fork and he starts eating");
+
+        wait();
+        print("has finished his meal");
+        hungry_ = false; 
+    }   
 
     void contemplate()
     {
-        std::stringstream ss;
-        ss << name_ << "is contemplating\n";
-        std::cout << ss.str();
-        std::this_thread::sleep_for(std::chrono::milliseconds(std::rand() % 3000));
+        wait();
+        print("is contemplating");
+        wait();
     }
 
-    Philosopher(const std::string &name, int left, int right)
-        : name_(name), forkIndexLeft_(left), forkIndexRight_(right) { }
+    void wait()
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(std::rand() % 2000));
+    }
+
+    void print(std::string str)
+    {
+        std::stringstream ss;
+        ss << "F" << id_ << " " << str << std::endl;
+        std::cout << ss.str();
+    }
+
+    Philosopher(int id, int left, int right)
+        : id_(id), forkIndexLeft_(left), forkIndexRight_(right) { }
 };
 
 int main()
 {
-    size_t n = 5;
     std::vector<std::thread> threads(n);
+    std::vector<Philosopher> philosophers;
 
-    std::vector<Philosopher> philosophers {
-            {"Tales", 0, 1          },
-            {"Pitagoras", 1, 2      },
-            {"Sokrates", 2, 3       },
-            {"Platon", 3, 4         },
-            {"Arystoteles", 4, 0    }
-    };
+    for (size_t i = 0; i < n - 1; i++)
+        philosophers.emplace_back(i, i, i+1);
+    philosophers.emplace_back(n - 1, n - 1, 0);
 
-    for (int i = 0; i < n; i++)
+    for (size_t i = 0; i < n; i++)
         threads[i] = std::thread(&Philosopher::dine, &philosophers[i]);
 
     for (auto &&i : threads)
