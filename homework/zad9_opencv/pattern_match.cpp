d8122360dcd8941ebfe603fbc4d64729430d0b0e
mkijew <mkijew@poczta.fm>
2020-03-11 12:35:54 +0100

    zad9: pattern match - seq ver2

--- homework/zad9_opencv/pattern_match.cpp
+++ homework/zad9_opencv/pattern_match.cpp
@@ -36,32 +36,36 @@ int main( int argc, char** argv )
     cv::Mat img_display;
     imgSource.copyTo( img_display );
 
-    // Create the result matrix
-    const int result_cols =  imgSource.cols - imgPatterns[0].cols + 1;
-    const int result_rows = imgSource.rows - imgPatterns[0].rows + 1;
-
     cv::Mat result;
-    result.create( result_rows, result_cols, CV_32FC1 );
 
-    // match pattern and normalize
-    cv::matchTemplate(imgSource, imgPatterns[0], result, cv::TM_SQDIFF);
-    cv::normalize(result, result, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
+    for( int i = 0; i < patternsCnt; ++i )
+    {
+        // Create the result matrix
+        const int result_cols =  imgSource.cols - imgPatterns[i].cols + 1;
+        const int result_rows = imgSource.rows - imgPatterns[i].rows + 1;
 
-    // localize the best match
-    double minVal; 
-    double maxVal; 
-    cv::Point minLoc; 
-    cv::Point maxLoc;
-    cv::Point matchLoc;
+        result.create( result_rows, result_cols, CV_32FC1 );
 
-    cv::minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, cv::Mat() );
-    matchLoc = minLoc;
+        // match pattern and normalize
+        cv::matchTemplate(imgSource, imgPatterns[i], result, cv::TM_SQDIFF);
+        cv::normalize(result, result, 0, 1, cv::NORM_MINMAX, -1, cv::Mat());
 
-    // show results
-    cv::rectangle( img_display, matchLoc, cv::Point( matchLoc.x + imgPatterns[0].cols , matchLoc.y + imgPatterns[0].rows ), cv::Scalar::all(0), 2, 8, 0 );
-    cv::rectangle( result, matchLoc, cv::Point( matchLoc.x + imgPatterns[0].cols , matchLoc.y + imgPatterns[0].rows ), cv::Scalar::all(0), 2, 8, 0 );
+        // localize the best match
+        double minVal;
+        double maxVal;
+        cv::Point minLoc;
+        cv::Point maxLoc;
+        cv::Point matchLoc;
 
-    // cv::imshow(image_window, imgSource);
+        cv::minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, cv::Mat() );
+        matchLoc = minLoc;
+
+        // show results
+        cv::rectangle( img_display, matchLoc, cv::Point( matchLoc.x + imgPatterns[i].cols , matchLoc.y + imgPatterns[0].rows ), cv::Scalar::all(0), 2, 8, 0 );
+        cv::rectangle( result, matchLoc, cv::Point( matchLoc.x + imgPatterns[i].cols , matchLoc.y + imgPatterns[0].rows ), cv::Scalar::all(0), 2, 8, 0 );
+    }
+
+    cv::imshow( image_window, imgSource);
     cv::imshow( image_window, img_display );
     cv::imshow( result_window, result );
 
