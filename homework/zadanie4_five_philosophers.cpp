2b75cdf31beb986ce14092ac48caf989cd1c07a4
piotrkowandy <piotr.kowandy@o2.pl>
2020-03-18 11:29:27 +0100

    new delete poprawione, wreszcie brak wyciekow pamieci

--- homework/zadanie4_five_philosophers.cpp
+++ homework/zadanie4_five_philosophers.cpp
@@ -17,12 +17,20 @@ public:
  
 class CTable{
 
+    int numbers_of_forks_;
+
 public:
     std::vector<CFork*> forks_pool_;
 
-    CTable(int number_of_forks){
-        for (size_t i=0; i < number_of_forks; i++){
+    CTable(int number_of_forks) : numbers_of_forks_(number_of_forks){
+        for (size_t i=0; i < numbers_of_forks_; i++){
             forks_pool_.push_back(new CFork());
+        }
+    }
+
+    ~CTable(){
+        for (int i = 0; i < numbers_of_forks_; i++){
+            delete forks_pool_[i];
         }
     }
 };
@@ -41,7 +49,6 @@ public:
     }
 
     ~CPhilosoph(){
-        delete this;
     }
 
     void eat(){
@@ -73,6 +80,7 @@ int main(int argc, char* argv[]){
     CTable table(numbers_of_philosophers);
     std::vector<std::thread> thread_pool(numbers_of_philosophers);
     std::vector<std::string> names_of_philosophers(numbers_of_philosophers);
+    std::vector<CPhilosoph*> philo_pointers;
 
     for (int i=0; i< numbers_of_philosophers; i++){
         names_of_philosophers[i] = "Philo" + std::to_string(i);
@@ -80,17 +88,22 @@ int main(int argc, char* argv[]){
 
     for (size_t i=0; i < numbers_of_philosophers; i++){
         if (i == 0){
-            thread_pool[i] = std::thread(&CPhilosoph::party, new CPhilosoph(table, names_of_philosophers[i], table.forks_pool_[i], table.forks_pool_[numbers_of_philosophers-1]));
+            philo_pointers.push_back(new CPhilosoph(table, names_of_philosophers[i], table.forks_pool_[i], table.forks_pool_[numbers_of_philosophers-1]));
+            thread_pool[i] = std::thread(&CPhilosoph::party, philo_pointers[i]);
         }
         else{
-            thread_pool[i] = std::thread(&CPhilosoph::party, new CPhilosoph(table, names_of_philosophers[i], table.forks_pool_[i-1], table.forks_pool_[i]));
+            philo_pointers.push_back(new CPhilosoph(table, names_of_philosophers[i], table.forks_pool_[i-1], table.forks_pool_[i]));
+            thread_pool[i] = std::thread(&CPhilosoph::party, philo_pointers[i]);
         }
     }
     for (auto&& th : thread_pool){
         th.join();
     }
 
+    for (int i=0; i < numbers_of_philosophers; i++){
+        delete philo_pointers[i];
+    }
+
     return 0;
 }
-
 
