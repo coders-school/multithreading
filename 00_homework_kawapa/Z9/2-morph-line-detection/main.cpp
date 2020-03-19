1a859fef3c0d1399c6d2d8cd0dd263c0b20616b4
kawapa <kawapa@outlook.com>
2020-03-19 20:51:11 +0100

    Exercise 9.2 - without colours

--- 00_homework_kawapa/Z9/2-morph-line-detection/main.cpp
+++ 00_homework_kawapa/Z9/2-morph-line-detection/main.cpp
@@ -9,6 +9,9 @@
 
 using namespace cv;
 
+constexpr size_t n = 4; // number of kernels
+
+void extractLines(Mat & inputOutput, const Mat & kernel);
 void prepare(const Mat &, Mat &, Mat &);
 void show_wait_destroy(const char* winname, cv::Mat img);
 
@@ -29,66 +32,46 @@ int main(int argc, char** argv)
 
     prepare(src, gray, bw);
     
-    std::array<const char*, 4> directions {"horizontal", "vertical", "diagonalLeft", "diagonalRight"};
-    std::vector<Mat> mats(4);
-    std::for_each(mats.begin(), mats.end(), [=](Mat & obj){ obj = bw.clone(); });
-    std::vector<std::future<void>> futures(4);
+    std::array<const char*, n> directions {"horizontal", "vertical", "diagonalLeft", "diagonalRight"};
+    std::vector<std::future<void>> futures(n);
 
-    auto horizontal = [&](cv::Mat &source){
-        int horizontal_size = source.cols / 30;
+    std::vector<cv::Mat> results(n);
+    std::for_each(results.begin(), results.end(), [=](Mat & obj){ obj = bw.clone(); });
+    
+    std::vector<cv::Mat> kernels;
+    kernels.reserve(n);
 
-        cv::Mat horizontalStructure = getStructuringElement(MORPH_RECT, Size(horizontal_size, 1));
+    int side = bw.rows / 30;
+    int arrayLeft[side * side] = {}; // matrix for left diagonal
+    int arrayRight[side * side] = {}; // matrix for right diagonal
 
-        erode(source, source, horizontalStructure, Point(-1, -1));
-        dilate(source, source, horizontalStructure, Point(-1, -1));
-    };
-    auto vertical = [&](cv::Mat &source) {
-        int vertical_size = source.rows / 30;
+    for (size_t i = 0; i < side * side; i = i + side + 1)
+        arrayLeft[i] = 1;
+    
+    for (size_t i = side - 1; i < side * side; i = i + side - 1)
+        arrayRight[i] = 1;
 
-        Mat verticalStructure = getStructuringElement(MORPH_RECT, Size(1, vertical_size));
+    kernels.emplace_back(getStructuringElement(MORPH_RECT, Size((bw.cols / 30), 1)));
+    kernels.emplace_back(getStructuringElement(MORPH_RECT, Size(1, (bw.rows / 30))));
+    kernels.emplace_back(Mat(side, side, CV_8U, arrayLeft));
+    kernels.emplace_back(Mat(side, side, CV_8U, arrayRight));
 
-        erode(source, source, verticalStructure, Point(-1, -1));
-        dilate(source, source, verticalStructure, Point(-1, -1));
-    };
+    for (size_t i = 0; i < n; i++)
+        futures[i] = std::async(std::launch::async, extractLines, std::ref(results[i]), std::cref(kernels[i]));
 
-    auto diagonalLeft = [&](cv::Mat &source) {
-        int side = source.rows / 30;
-        int array[side * side] = {};
-
-        for (size_t i = 0; i < side * side; i = i + side + 1)
-            array[i] = 1;
-
-        Mat diagonalLeftStructure = Mat(side, side, CV_8U, array);
-
-        erode(source, source, diagonalLeftStructure, Point(-1, -1));
-        dilate(source, source, diagonalLeftStructure, Point(-1, -1));
-    };
-
-    auto diagonalRight = [&](cv::Mat &source) {
-        int side = source.rows / 30;
-        int array[side * side] = {};
-
-        for (size_t i = side - 1; i < side * side; i = i + side - 1)
-            array[i] = 1;
-
-        Mat diagonalRightStructure = Mat(side, side, CV_8U, array);
-
-        erode(source, source, diagonalRightStructure, Point(-1, -1));
-        dilate(source, source, diagonalRightStructure, Point(-1, -1));
-    };
-
-    futures[0] = std::async(std::launch::async, horizontal, std::ref(mats[0]));
-    futures[1] = std::async(std::launch::async, vertical, std::ref(mats[1]));
-    futures[2] = std::async(std::launch::async, diagonalLeft, std::ref(mats[2]));
-    futures[3] = std::async(std::launch::async, diagonalRight, std::ref(mats[3]));    
-
-    for (int i = 0; i < 4; i++)
-    {
-        futures[i].get();
-        show_wait_destroy(directions[i], mats[i]);
-    }  
+    std::for_each(futures.begin(), futures.end(), [](std::future<void> & obj){ obj.get(); });
+           
+    auto finalResult = results[0] + results[1] + results[2] + results[3];
+    show_wait_destroy("Result", finalResult);
 
     return 0;
+}
+
+void extractLines(Mat & inputOutput, const Mat & kernel)
+{
+    erode(inputOutput, inputOutput, kernel, Point(-1, -1));
+    dilate(inputOutput, inputOutput, kernel, Point(-1, -1));
+    cvtColor(inputOutput, inputOutput, COLOR_GRAY2BGR);
 }
 
 void prepare(const Mat & src, Mat & gray, Mat & bw)
