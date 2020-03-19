1ec765d074e327bb0fd99ef0fdff8ac990495722
kawapa <kawapa@outlook.com>
2020-03-19 14:30:42 +0100

    Exercise 9.2 - separate results

--- 00_homework_kawapa/Z9/2-morph-line-detection/main.cpp
+++ 00_homework_kawapa/Z9/2-morph-line-detection/main.cpp
@@ -29,25 +29,65 @@ int main(int argc, char** argv)
 
     prepare(src, gray, bw);
     
-    auto diagonalLeft = [&](cv::Mat &source){
+    std::array<const char*, 4> directions {"horizontal", "vertical", "diagonalLeft", "diagonalRight"};
+    std::vector<Mat> mats(4);
+    std::for_each(mats.begin(), mats.end(), [=](Mat & obj){ obj = bw.clone(); });
+    std::vector<std::future<void>> futures(4);
 
-                int side = source.rows / 30;
-                int array[side*side] = {};
+    auto horizontal = [&](cv::Mat &source){
+        int horizontal_size = source.cols / 30;
 
-                for (size_t i = 0; i < side * side; i = i + side + 1)
-                    array[i] = 1;
+        cv::Mat horizontalStructure = getStructuringElement(MORPH_RECT, Size(horizontal_size, 1));
 
-                Mat diagonalLeftStructure = Mat(side, side, CV_8U, array);
+        erode(source, source, horizontalStructure, Point(-1, -1));
+        dilate(source, source, horizontalStructure, Point(-1, -1));
+    };
+    auto vertical = [&](cv::Mat &source) {
+        int vertical_size = source.rows / 30;
 
-                erode(source, source, diagonalLeftStructure, Point(-1, -1));
-                dilate(source, source, diagonalLeftStructure, Point(-1, -1));
+        Mat verticalStructure = getStructuringElement(MORPH_RECT, Size(1, vertical_size));
+
+        erode(source, source, verticalStructure, Point(-1, -1));
+        dilate(source, source, verticalStructure, Point(-1, -1));
     };
 
-    Mat empty = bw.clone();
-    std::future<void> futures = std::async(std::launch::async, diagonalLeft, std::ref(empty));
-    futures.get();
-    show_wait_destroy("diagonalLeft", empty);
- 
+    auto diagonalLeft = [&](cv::Mat &source) {
+        int side = source.rows / 30;
+        int array[side * side] = {};
+
+        for (size_t i = 0; i < side * side; i = i + side + 1)
+            array[i] = 1;
+
+        Mat diagonalLeftStructure = Mat(side, side, CV_8U, array);
+
+        erode(source, source, diagonalLeftStructure, Point(-1, -1));
+        dilate(source, source, diagonalLeftStructure, Point(-1, -1));
+    };
+
+    auto diagonalRight = [&](cv::Mat &source) {
+        int side = source.rows / 30;
+        int array[side * side] = {};
+
+        for (size_t i = side - 1; i < side * side; i = i + side - 1)
+            array[i] = 1;
+
+        Mat diagonalRightStructure = Mat(side, side, CV_8U, array);
+
+        erode(source, source, diagonalRightStructure, Point(-1, -1));
+        dilate(source, source, diagonalRightStructure, Point(-1, -1));
+    };
+
+    futures[0] = std::async(std::launch::async, horizontal, std::ref(mats[0]));
+    futures[1] = std::async(std::launch::async, vertical, std::ref(mats[1]));
+    futures[2] = std::async(std::launch::async, diagonalLeft, std::ref(mats[2]));
+    futures[3] = std::async(std::launch::async, diagonalRight, std::ref(mats[3]));    
+
+    for (int i = 0; i < 4; i++)
+    {
+        futures[i].get();
+        show_wait_destroy(directions[i], mats[i]);
+    }  
+
     return 0;
 }
 
