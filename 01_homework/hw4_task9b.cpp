5e92c585ebb4f31ec0b8c235e79b8ae87e2f4999
Jakub Zych <jzych89@gmail.com>
2020-03-25 23:47:14 +0100

    Add diagonal line detection

--- 01_homework/hw4_task9b.cpp
+++ 01_homework/hw4_task9b.cpp
@@ -7,11 +7,35 @@
 using namespace std;
 using namespace cv;
 
+constexpr int angle_step = 5;
+
 void show_wait_destroy(const char* winname, cv::Mat img) {
     imshow(winname, img);
     moveWindow(winname, 500, 0);
     waitKey(0);
     destroyWindow(winname);
+}
+
+Mat get_diagonal_line_mask(int line_size, int angle) {
+    double angle_rad = ( angle * M_PI ) / 180.0;
+
+    // Create mask of proper size
+    Mat test_image(abs((line_size * cos(angle_rad))) + 1,
+                   abs((line_size * sin(angle_rad))) + 1,
+                   CV_8U);
+
+    // Create rotated rectagle and width = 0
+    RotatedRect rRect = RotatedRect(Point2f(abs((line_size/2 * sin(angle_rad))),
+                                            abs((line_size/2 * cos(angle_rad)))),
+                                    Size2f(0, line_size),
+                                    angle);
+
+    // Get longer side as mask
+    Point2f vertices[4];
+    rRect.points(vertices);
+    line(test_image, vertices[0], vertices[1], Scalar(255,255,255), 1);
+
+    return test_image;
 }
 
 int main(int argc, char** argv)
@@ -43,47 +67,46 @@ int main(int argc, char** argv)
     Mat bw;
     adaptiveThreshold(~gray, bw, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 15, -2);
 
-    // Create the images that will use to extract the horizontal and vertical lines
-    Mat horizontal = bw.clone();
-    Mat vertical = bw.clone();
-
-    // Specify size on horizontal/vertical axis
-    int horizontal_size = horizontal.cols / 30;
-    int vertical_size = vertical.rows / 30;
-
-    // // Create structure element for extracting horizontal/vertical lines through morphology operations
-    // Mat horizontalStructure = getStructuringElement(MORPH_RECT, Size(horizontal_size, 1));
-    // Mat verticalStructure = getStructuringElement(MORPH_RECT, Size(1, vertical_size));
+    // Specify axis lenght
+    int line_lenght = ((src.cols / 30) + (src.rows / 30) / 2);
 
     // Apply morphology operations
-    auto morphFunction = [](Mat& bw_image, const int param_a, const int param_b)
+    auto morphFunction = [](Mat bw_image, const int line_lenght, const int angle)
     {
         // Create structure element for extracting horizontal/vertical lines through morphology operations
-        cv::Mat imgStructure = cv::getStructuringElement(cv::MORPH_RECT,
-                                                         Size(param_a, param_b));
+        Mat imgStructure = get_diagonal_line_mask(line_lenght, angle);
 
         // Apply morphology operations
-        cv::erode(bw_image, bw_image, imgStructure, cv::Point(-1, -1));
-        cv::dilate(bw_image, bw_image, imgStructure, cv::Point(-1, -1));
+        erode(bw_image, bw_image, imgStructure, Point(-1, -1));
+        dilate(bw_image, bw_image, imgStructure, Point(-1, -1));
 
         return bw_image;
     };
 
-    std::future<cv::Mat> horizontal_result = std::async(std::launch::async,
-                                                        morphFunction,
-                                                        std::ref(horizontal),
-                                                        horizontal_size,
-                                                        1);
-    std::future<cv::Mat> vertical_result = std::async(std::launch::async,
-                                                      morphFunction,
-                                                      std::ref(vertical),
-                                                      1,
-                                                      vertical_size);
+    vector<future<Mat>> results;
+    for( int angle = 0; angle < 180; angle += angle_step) {
+        // Launch thread for line matching
+        results.push_back(std::async(std::launch::async,
+                                     morphFunction,
+                                     bw.clone(),
+                                     line_lenght,
+                                     angle));
+    }
 
-    // Show extracted horizontal lines
-    show_wait_destroy("horizontal", horizontal_result.get());
+    // Coloring step helpers
+    int r_step = 256 / (180 / angle_step);
+    int g_step = r_step * 3;
+    int b_step = g_step * 3;
 
-    // Show extracted vertical lines
-    show_wait_destroy("vertical", vertical_result.get());
+    // Show extracted lines
+    Mat result_img = src.clone();
+    for(auto i = 0; i < static_cast<int>(results.size()); ++i) {
+        // Add detected lines to final result
+        result_img.setTo(cv::Scalar((i * r_step) % 255,
+                                    (i * g_step) % 255,
+                                    (i * b_step) % 255),
+                         results[i].get());
+    }
+    show_wait_destroy("result", result_img);
     return 0;
 }
