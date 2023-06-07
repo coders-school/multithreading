2265ccc98485ddd91eba4d379e249813dcb12f50
sebastian <59411002+diagnolek@users.noreply.github.com>
2023-06-07 06:36:45 +0200

    Update 03_methods_passing.cpp
    przekazanie danych w watku za pomoca metody setData

--- module1/exercises/03_methods_passing.cpp
+++ module1/exercises/03_methods_passing.cpp
@@ -18,7 +18,8 @@ public:
 
 int main() {
     Car toyota;
-    // set year to 2015, model to "Corolla" in a thread
+    thread t(&Car::setData, &toyota, 2011, "corolla");
+    t.join();
     toyota.print();
     return 0;
 }
